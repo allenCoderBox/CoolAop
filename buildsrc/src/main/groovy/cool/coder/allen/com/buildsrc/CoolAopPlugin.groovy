577511f1d3dfc824edc2ce30960d5d1d522a22f3
husongzhen <husongzhen@husongzhendeMacBook-Pro.local>
2017-11-28 23:22:46 +0800

    add dom

--- buildsrc/src/main/groovy/cool/coder/allen/com/buildsrc/CoolAopPlugin.groovy
+++ buildsrc/src/main/groovy/cool/coder/allen/com/buildsrc/CoolAopPlugin.groovy
@@ -1,8 +1,13 @@
 package cool.coder.allen.com.buildsrc
 
 import com.android.build.gradle.AppExtension
+import org.dom4j.Document
+import org.dom4j.Node
+import org.dom4j.io.SAXReader
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+
+
 /**
  * Created by husongzhen on 17/11/24.
  */
@@ -10,7 +15,43 @@ import org.gradle.api.Project
 class CoolAopPlugin implements Plugin<Project> {
     @Override
     void apply(Project project) {
+        String homeDir = project.getRootDir()
+        def libsDir = new File(homeDir + "/.idea/libraries")
+        libsDir.eachFile { file ->
+            project.logger.error(file.name)
+            SAXReader saxReader = new SAXReader()
+            Document document = saxReader.read(file)
+
+
+
+            List<Node> nodes = document.selectNodes("//component/library/CLASSES")
+            nodes.each { node ->
+
+                project.logger.error(node.selectSingleNode("root").asXML())
+            }
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        assert "a" == "b"
+
+
+
         project.logger.error "================自定义插件start！=========="
+        project.logger.error project.rootDir.absolutePath
+
         def android = project.extensions.findByType(AppExtension)
         def transform = new AopTransform(project);
         android.registerTransform(transform)
